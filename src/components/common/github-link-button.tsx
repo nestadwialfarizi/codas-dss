cfe9e156268289a4c9e75f287dd9268788f723bc
Nesta Dwi Alfarizi <nestadwia@gmail.com>
2024-03-15 08:52:05 +0700

    github button target blank

--- src/components/common/github-link-button.tsx
+++ src/components/common/github-link-button.tsx
@@ -1,10 +1,10 @@
+import Link from 'next/link';
 import { GitHubLogoIcon } from '@radix-ui/react-icons';
 import { Button } from '../ui/button';
-import Link from 'next/link';
 
 export function GitHubLinkButton() {
   return (
-    <Link href='https://github.com/nestadwialfarizi/codas-dss'>
+    <Link href='https://github.com/nestadwialfarizi/codas-dss' target='_blank'>
       <Button variant='outline' size='icon'>
         <GitHubLogoIcon className='h-4 w-4' />
       </Button>
