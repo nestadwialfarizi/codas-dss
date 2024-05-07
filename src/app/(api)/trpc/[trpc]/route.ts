83358fef5059d370d3a9a883103e6e0d372f0c60
Nesta Dwi Alfarizi <nestadwia@gmail.com>
2024-05-07 19:32:58 +0700

    fix error create trpc context on build

--- src/app/(api)/trpc/[trpc]/route.ts
+++ src/app/(api)/trpc/[trpc]/route.ts
@@ -8,7 +8,7 @@ function handler(request: NextRequest) {
     endpoint: '/trpc',
     req: request,
     router: router,
-    createContext: () => createContext({ req: request }),
+    createContext,
   });
 }
 
