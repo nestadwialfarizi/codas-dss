5ce87d63f179a9dacaae4d2fb63d65e14200978a
Nesta Dwi Alfarizi <nestadwia@gmail.com>
2024-05-07 11:17:39 +0700

    Install nextjs and setup shadcn-ui

---
+++ src/lib/config.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  name: 'Sistem Pendukung Keputusan CODAS',
+  description: 'Sistem Pendukung Keputusan metode CODAS',
+  faviconUrl: '/favicon.ico',
+};
+
+export type SiteConfig = typeof siteConfig;
