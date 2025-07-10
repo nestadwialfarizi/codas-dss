3e24286204f30a7dc282466b93220b0cc315225f
Nesta Dwi Alfarizi <nestadwia@gmail.com>
2025-07-10 18:48:04 +0700

    Update layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -7,12 +7,12 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
     <main className='flex flex-row'>
       <div className='container hidden h-screen flex-col items-center justify-center ps-16 text-center lg:flex lg:basis-3/4'>
         <h1 className='text-5xl font-bold tracking-tight'>
-          Temukan Rekomendasi Digital Multi-Effects terbaik anda
+          Sistem Pendukung Keputusan Pemilihan Smartphone Gaming Mid-range Terbaik
         </h1>
         <p className='text-muted-foreground'>
           Sistem Pendukung Keputusan berbasis website dengan menerapkan metode
           analisis Combinative Distance-based Assessment (CODAS) dalam
-          menentukan rekomendasi terbaik alat audio Digital Multi-Effects.
+          menentukan rekomendasi terbaik Smartphone Gaming Mid-range.
         </p>
       </div>
       <div className='flex h-screen basis-full items-center justify-center lg:basis-1/2'>
